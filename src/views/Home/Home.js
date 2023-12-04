089ca85bb7fc849f9a5101d523af743fa38101fc
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-12-04 20:46:31 +0530

    NEW FILES AND TEXT ADDED

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,7 +3,15 @@ import "./Home.css"
 
 function Home(){
     return(
-        <div>home</div>
+        <div>
+            <h1>welcome</h1>
+
+            <p>
+                we hope you enjoy learning react 
+            </p>
+
+            <h3>have a great day</h3>
+        </div>
     )
 }
 
