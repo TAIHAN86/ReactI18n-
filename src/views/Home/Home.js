e65f9b33b51b6278af136ac817619eac802b4e61
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-12-06 19:46:16 +0530

    function created in home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,17 +1,31 @@
-import React from "react"
-import "./Home.css"
-
+import React from 'react';
+import './Home.css';
+import I18n from '../../utils/I18n';
 function Home(){
-    return(
-        <div>
-            <h1>welcome</h1>
 
-            <p>
-                we hope you enjoy learning react 
-            </p>
+    const users=15;
 
-            <h3>have a great day</h3>
-        </div>
+
+    return (
+        <>
+  <h1>{I18n("welcomemessage")}</h1>
+
+  <p>{I18n("normalmessage")}</p>
+  
+  <h3>{I18n("greetingmessage")}</h3>
+<select 
+defaultValue={localStorage.getItem("lang")}
+onChange={(e)=>{
+    localStorage.setItem("lang",e.target.value);
+    window.location.reload();
+}}>
+    <option value="mr">Marathi</option>
+    <option value="hi">Hindi</option>
+    <option value="en">English</option>
+</select>
+
+  <p>{users} {I18n("Userscount")}</p>
+  </>
     )
 }
 
