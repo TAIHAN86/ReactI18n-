a1f7e1ce075e050a8d544e9f5c62f7cd553091cc
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-12-07 21:41:15 +0530

    css added to text

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import './Home.css';
-import I18n from './src/I18n.js';
-function Home(){
+import I18n from '../../components/I18n';
 
-    const users=15;
-
+function Home() {
+    const users = 15;
 
     return (
         <>
-  <h1>{I18n("welcomemessage")}</h1>
+            
+            <h1 className="main-heading">{I18n("welcomemessage")}</h1>
+            <p className="normal-text">{I18n("normalmessage")}</p>
+            <h3 className="sub-heading">{I18n("greetingmessage")}</h3>
 
-  <p>{I18n("normalmessage")}</p>
-  
-  <h3>{I18n("greetingmessage")}</h3>
-<select 
-defaultValue={localStorage.getItem("lang")}
-onChange={(e)=>{
-    localStorage.setItem("lang",e.target.value);
-    window.location.reload();
-}}>
-    <option value="mr">Marathi</option>
-    <option value="hi">Hindi</option>
-    <option value="en">English</option>
-</select>
 
-  <p>{users} {I18n("Userscount")}</p>
-  </>
-    )
+            <select
+                className="language-select"
+                defaultValue={localStorage.getItem("lang")}
+                onChange={(e) => {
+                    localStorage.setItem("lang", e.target.value);
+                    window.location.reload();
+                }}
+            >
+                <option value="mr" className="marathi-option">Marathi</option>
+                <option value="hi" className="hindi-option">Hindi</option>
+                <option value="en" className="english-option">English</option>
+            </select>
+
+            <p>{users} {I18n("Userscount")}</p>
+        </>
+    );
 }
 
-export default Home
+export default Home;
